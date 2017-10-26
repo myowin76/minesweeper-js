48ee6b20f11326abd6c84b6780b6709c294f1dd2
Aung Myo Win <aungmyowin@akauk.com>
2017-10-26 15:56:23 +0100

    final game board updated - still need to update notification

---
+++ src/game.js
@@ -0,0 +1,34 @@
+// To play Minesweeper, we will create instances of MineSweeperGame in command line.
+// For example:
+// In the command line, navigate to the lib directory and run `node`
+// Run `.load game.js` to load the contents of this file.
+// Then create a Game instance and run commands like so:
+// let game = new Game(3, 3, 3);
+// game.playMove(0, 1);
+// game.playMove(1, 2);
+// When done run `.exit`
+
+import { Board } from './board';
+
+class Game {
+	constructor(numberOfRows, numberOfColumns, numberOfBombs){
+		this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs)
+	}
+
+	playMove(rowIndex, columnIndex){
+		this._board.flipTile(rowIndex, columnIndex)
+		if (this._board._playerBoard[rowIndex, columnIndex] === 'B' ){
+			console.log('Catch the Bomb, the game is over');
+			
+		}else if(this.hasSafeTiles) {
+			console.log('Congratulation, You won!');
+
+		}else{
+			console.log('Current Board:');
+			// console.log(this._board);
+
+		}
+
+		this._board.print();
+	}
+} //class
